e37c4d3cc2e7d1d95dd6cee928f05504e40bbf37
Devin Sarao <devin.sarao@patelhs.org>
2020-11-12 20:25:57 +0000

    Added brackets for my for loop and print statement

--- Main.java
+++ Main.java
@@ -4,7 +4,9 @@ class Main {
    int[] seats = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
    String[] studentsName = new String[15];
    Scanner scan = new Scanner(System.in); 
-   for ( int i = 0; i < studentsName.length; i++)
+   for ( int i = 0; i < studentsName.length; i++){
    System.out.println("Please type your name.");
+   studentsName [i] = scan.next();}
+   
   }
 }
