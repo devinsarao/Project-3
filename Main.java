3e149212feff8c7b7ba290f73bb610f54576460a
Devin Sarao <devin.sarao@patelhs.org>
2020-11-12 20:03:25 +0000

    I added the scanner for my code.

--- Main.java
+++ Main.java
@@ -3,5 +3,6 @@ class Main {
   public static void main(String[] args) {
    int[] seats = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
    String[] names = new String[15];
+   Scanner scan = new Scanner(System.in); 
   }
 }
